9ce2b6524d969d8d003c5661fc435b7fc7794b06
hash_developer <63818411+davidesidoti@users.noreply.github.com>
2023-04-23 03:02:32 +0200

    New AI

--- test.py
+++ test.py
@@ -1,57 +1,82 @@
-import json
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+import threading
+import datetime
+import random
 
-# Load the JSON dataset
-with open('./datasets/commands.json') as f:
-    dataset = json.load(f)
+import speech_recognition
+import pyttsx3 as tts
 
-# Extract the texts and intent labels from the dataset
-texts = [d['text'] for d in dataset]
-intent_labels = [d['intent'] for d in dataset]
+from neuralintents import GenericAssistant
 
-# Preprocess an input sentence
-input_sentence = input('Command: ')
-# Tokenize the sentence
-input_tokens = input_sentence.split()
-# Convert the tokens to lowercase
-input_tokens = [token.lower() for token in input_tokens]
-# Remove stopwords (optional)
-stopwords = set(ENGLISH_STOP_WORDS)
-input_tokens = [token for token in input_tokens if token not in stopwords]
-# Join the tokens back into a single string
-input_text = ' '.join(input_tokens)
 
-# Extract features from the input sentence
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform([input_text] + texts)
+class Nova:
+    def __init__(self):
+        self.recognizer = speech_recognition.Recognizer()
+        self.speaker = tts.init(driverName='sapi5')
+        self.speaker.setProperty("rate", 150)
+        self.speaker.setProperty(
+            "voice", self.speaker.getProperty('voices')[1].id)
 
-# Train a logistic regression classifier
-clf = LogisticRegression()
-clf.fit(X[1:], intent_labels)
+        self.assistant = GenericAssistant(
+            "datasets/intents.json", intent_methods={"time": self.get_the_time})
+        self.assistant.train_model()
 
-# Get prediction accuracy
-predicted_prob = clf.predict_proba(X[:1])[0]
+        # threading.Thread(target=self.run_assistant).start()
+        self.run_assistant()
 
-# Make a prediction on the input sentence
-predicted_intent = clf.predict(X[:1])[0]
+    def get_the_time(self):
+        responses = [
+            "The time is %%.",
+            "It is currently %%.",
+            "The current time is %%.",
+            "It's approximately %%.",
+            "Let me check... the time now is %%.",
+            "I have the time, it's %%."
+        ]
 
-# Define a threshold or confidence score below which the prediction is uncertain
-threshold = 0.6
-print(predicted_prob.max())
+        # Get the current time
+        now = datetime.datetime.now().strftime("%H:%M")
 
-if predicted_prob.max() < threshold:
-    # The prediction is uncertain and Halo asks for the user intent
-    print("Sorry, as an AI I'm still learning. Can you tell me what was your intent with the last sentence you said?")
-    # Add the new sentence and intent to the dataset
-    with open('./datasets/commands.json', 'w') as f:
-        new_entry = {
-            "text": str(input_sentence.lower().replace('halo', '')),
-            "intent": input('Intent: ')
-        }
-        dataset.append(new_entry)
-        json.dump(dataset, f)
-else:
-    # The prediction is confident
-    print(f"The intent is {predicted_intent}.")
+        response = random.choice(responses).replace("%%", now)
+        self.speaker.say(response)
+        self.speaker.runAndWait()
+
+    def run_assistant(self):
+        while True:
+            try:
+                with speech_recognition.Microphone() as mic:
+                    print("Mic active.")
+                    self.recognizer.pause_threshold = 1
+                    self.recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                    audio = self.recognizer.listen(mic)
+
+                    text = self.recognizer.recognize_google(audio)
+                    text = text.lower()
+
+                    print("before awake word")
+                    # Nova
+                    if "nova" in text:
+                        print("nova activated. Speak now...")
+                        audio = self.recognizer.listen(mic)
+                        text = self.recognizer.recognize_google(audio)
+                        text = text.lower()
+                        if text == "stop":
+                            self.speaker.say("Bye!")
+                            self.speaker.runAndWait()
+                            self.speaker.stop()
+                            exit()
+                        else:
+                            if text is not None:
+                                response = self.assistant.request(text)
+                                if response is not None:
+                                    print(response)
+                                    self.speaker.say(response)
+                                    self.speaker.runAndWait()
+                    else:
+                        print("Text: ", text)
+                    print("after awake word")
+            except Exception as e:
+                print("An error occurred: ", str(e))
+                continue
+
+
+Nova()
